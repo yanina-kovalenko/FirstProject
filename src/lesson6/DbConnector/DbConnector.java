23e4311b389e9e68951516cd14b32a36a73df4e6
yanina-kovalenko <kovalenko.yanina.1994@gmail.com>
2020-12-02 20:34:45 +0200

    lesson6

---
+++ src/lesson6/DbConnector/DbConnector.java
@@ -0,0 +1,12 @@
+package lesson6.DbConnector;
+
+public class DbConnector {
+    String host;
+    int port;
+    String userName;
+    String password;
+
+    void connect() {
+        //logic
+    }
+}
