baf013b2d0fe5756efdfc2e07ae7410de5f1f323
yanina-kovalenko <kovalenko.yanina.1994@gmail.com>
2020-12-08 00:41:54 +0200

    lesson9.Encapsulation

---
+++ src/lesson9/Company.java
@@ -0,0 +1,31 @@
+package lesson9;
+
+ public class Company {
+    private String name;
+    private String countryFounded;
+
+    protected String code;
+
+    public int someField;
+
+     public Company(String name, String countryFounded) {
+         this.name = name;
+         this.countryFounded = countryFounded;
+     }
+
+     public String getName() {
+         return name;
+     }
+
+//     public void setName(String name) {
+//         this.name = name;
+//     }
+
+     public String getCountryFounded() {
+         return countryFounded;
+     }
+
+     public void setCountryFounded(String countryFounded) {
+         this.countryFounded = countryFounded;
+     }
+ }
---
+++ src/lesson9/Demo.java
@@ -0,0 +1,18 @@
+package lesson9;
+
+import lesson9.utils.Checker;
+
+public class Demo {
+    public static void main(String[] args) {
+        Company company = new Company("Yana", "Ukraine");
+        System.out.println(company.getCountryFounded());
+        System.out.println(company.getName());
+
+//        company.name = "IBM";
+
+        Checker checker = new Checker();
+        checker.checkCompanyName(company.getName());
+        System.out.println(checker.checkCompanyName(company.getName()));
+//        System.out.println(checker.companyNamesValidatedCount);
+    }
+}
