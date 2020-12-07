7a945b2f3cf51d1b92c93924839b2a0cd6a1b4bd
yanina-kovalenko <kovalenko.yanina.1994@gmail.com>
2020-12-08 01:03:53 +0200

    Update Demo.java
    parameters are removed

--- src/lesson9/Demo.java
+++ src/lesson9/Demo.java
@@ -4,7 +4,7 @@ import lesson9.utils.Checker;
 
 public class Demo {
     public static void main(String[] args) {
-        Company company = new Company("Yana", "Ukraine");
+        Company company = new Company();
         System.out.println(company.getCountryFounded());
         System.out.println(company.getName());
 
