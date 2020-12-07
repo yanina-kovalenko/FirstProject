1649e5e480065a2f6ca368e41a7006567026b8e6
yanina-kovalenko <kovalenko.yanina.1994@gmail.com>
2020-12-08 01:05:59 +0200

    Update Demo.java
    fix5

--- src/lesson9/Demo.java
+++ src/lesson9/Demo.java
@@ -5,16 +5,16 @@ import lesson9.utils.Checker;
 public class Demo {
     public static void main(String[] args) {
         Company company = new Company();
-        System.out.println(company.getCountryFounded());
+        System.out.println(company.countryFounded);
         System.out.println(company.getName());
 
-//        company. = "New Value";
+       company. = "New Value";
 
-//        company.name = "IBM";
+       company.name = "IBM";
 
         Checker checker = new Checker();
-        checker.checkCompanyName(company.getName());
-        System.out.println(checker.checkCompanyName(company.getName()));
+        checker.checkCompanyName(company.name);
+        System.out.println(checker.checkCompanyName(company.name));
 //        System.out.println(checker.companyNamesValidatedCount);
     }
 }
