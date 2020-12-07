21d0fc82e499cb1ca53af86ae936e8f2b9e20020
yanina-kovalenko <kovalenko.yanina.1994@gmail.com>
2020-12-08 00:56:31 +0200

    Update Demo.java
    fix3

--- src/lesson9/Demo.java
+++ src/lesson9/Demo.java
@@ -4,17 +4,17 @@ import lesson9.utils.Checker;
 
 public class Demo {
     public static void main(String[] args) {
-        Company company = new Company();
-        System.out.println(company.countryFounded);
+        Company company = new Company("Yana", "Ukraine");
+        System.out.println(company.getCountryFounded());
         System.out.println(company.getName());
-        
-        company. = "New Value";
 
-        company.name = "IBM";
+//        company. = "New Value";
+
+//        company.name = "IBM";
 
         Checker checker = new Checker();
-        checker.checkCompanyName(company.name);
-        System.out.println(checker.checkCompanyName(company.name));
+        checker.checkCompanyName(company.getName());
+        System.out.println(checker.checkCompanyName(company.getName()));
 //        System.out.println(checker.companyNamesValidatedCount);
     }
 }
