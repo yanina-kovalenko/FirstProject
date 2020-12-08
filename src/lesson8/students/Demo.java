117d40901b5285de0f55b519b6647a544dbe4fd2
yanina-kovalenko <kovalenko.yanina.1994@gmail.com>
2020-12-08 14:11:47 +0200

    Update Demo.java
    updated

--- src/lesson8/students/Demo.java
+++ src/lesson8/students/Demo.java
@@ -4,12 +4,12 @@ public class Demo {
     public Student createHighestParent() {
 //       Course[] coursesTaken = new Course[7];
 
-        Student student = new Student("Yanina", "Kovalenko", 5, createHighestParent().coursesTaken);
+        Student student = new Student("Yanina", "Kovalenko", 5, null);
         return student;
     }
 
     public SpecialStudent createLowestChild() {
-        SpecialStudent specialStudent = new SpecialStudent("Vladislav", "Luchko", 6, createLowestChild().coursesTaken, 1234567, "vlad@gmail.com");
+        SpecialStudent specialStudent = new SpecialStudent("Vladislav", "Luchko", 6, null, 1234567, "vlad@gmail.com");
         return specialStudent;
     }
 }
